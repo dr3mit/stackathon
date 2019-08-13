36e637d504b9007cf74a9298ac372f9534fead2e
Drew <dr3mit@gmail.com>
2019-08-12 21:30:47 -0400

    update unit model and constants 0

--- backEnd/models/Unit.js
+++ backEnd/models/Unit.js
@@ -1,27 +1,69 @@
 const db = require('../db');
 const Sequelize = require('sequelize');
+const {
+  TINY,
+  SMALL,
+  STANDARD,
+  LARGE,
+  HEAVY_INFANTRY,
+  SWORD,
+} = require('../../constants');
 
 const Unit = db.define('unit', {
   name: {
     type: Sequelize.STRING,
   },
-  size,
-  numOfModels,
-  speed,
-  type,
-  weapons,
-  upgrades,
-  curUpgrades,
-  cost,
-  curCost,
-  clash,
-  sustained,
-  shortRange,
-  longRange,
-  save,
-  stanima,
-  specialRules,
-  possibleSpecialRules,
+  size: {
+    type: Sequelize.ENUM([TINY, SMALL, STANDARD, LARGE]),
+  },
+  numOfModels: {
+    type: Sequelize.INTEGER,
+  },
+  speed: {
+    type: Sequelize.INTEGER,
+  },
+  type: {
+    type: Sequelize.ENUM([HEAVY_INFANTRY]),
+  },
+  weapons: {
+    type: Sequelize.ENUM([SWORD]),
+  },
+  upgrades: {
+    type: Sequelize.ARRAY,
+  },
+  curUpgrades: {
+    type: Sequelize.ARRAY,
+  },
+  cost: {
+    type: Sequelize.INTEGER,
+  },
+  curCost: {
+    type: Sequelize.INTEGER,
+  },
+  clash: {
+    type: Sequelize.INTEGER,
+  },
+  sustained: {
+    type: Sequelize.INTEGER,
+  },
+  shortRange: {
+    type: Sequelize.INTEGER,
+  },
+  longRange: {
+    type: Sequelize.INTEGER,
+  },
+  save: {
+    type: Sequelize.INTEGER,
+  },
+  stanima: {
+    type: Sequelize.INTEGER,
+  },
+  specialRules: {
+    type: Sequelize.ENUM(this.possibleSpecialRules),
+  },
+  possibleSpecialRules: {
+    type: Sequelize.ARRAY,
+  },
 });
 
 module.exports = Unit;
---
+++ constants.js
@@ -0,0 +1,10 @@
+const constants = {
+  TINY: 'TINY',
+  SMALL: 'SMALL',
+  STANDARD: 'STANDARD',
+  LARGE: 'LARGE',
+  HEAVY_INFANTRY: 'HEAVY_INFANTRY',
+  SWORD: 'SWORD',
+};
+
+module.exports = constants;
